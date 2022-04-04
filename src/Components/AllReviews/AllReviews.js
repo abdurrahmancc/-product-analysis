b34cfac2238b392a441a2554ea7b3e950921cd76
abdurrahmancc <personalbusinessmail63@gmail.com>
2022-04-05 03:54:48 +0600

    review added

--- src/Components/AllReviews/AllReviews.js
+++ src/Components/AllReviews/AllReviews.js
@@ -1,22 +1,34 @@
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Card } from "react-bootstrap";
+import "./AllReviews.css";
 
 const AllReviews = (props) => {
   const { name, image, comment, rating } = props.product;
 
   return (
     <div className="col-md-4">
-      <Card>
-        <Card.Img width="50px" height="100px" className="p-3 rounded-circle" src={image.url} />
+      <Card className="align-items-center">
+        <Card.Img
+          style={{ width: "100px" }}
+          className=" img-style rounded-circle"
+          src={image.url}
+        />
         <Card.Body>
-          <Card.Title>{name}</Card.Title>
-          <Card.Text className="text-start">{comment.slice(0, 150)}</Card.Text>
-          <div className="d-flex justify-content-between">
-            <span>Rating: {rating}</span>
-            <button title={comment} className="border-0 py-1 px-2 btn-color text-white rounded ">
-              More Details
-            </button>
+          <Card.Title className="fs-3">{name}</Card.Title>
+
+          <div className="mb-4">
+            <FontAwesomeIcon className="text-warning icons" icon={faStar} />
+            <FontAwesomeIcon className="text-warning icons" icon={faStar} />
+            <FontAwesomeIcon className="text-warning icons" icon={faStar} />
+            <FontAwesomeIcon className="text-warning icons" icon={faStar} />
+            <FontAwesomeIcon className="text-warning icons" icon={faStar} />
           </div>
+          <Card.Text className="text-start">
+            {comment.slice(0, 150)}
+            {/* <a href="">Read more</a> */}
+          </Card.Text>
         </Card.Body>
       </Card>
     </div>
--- src/Components/ProductsReview/ProductsReview.js
+++ src/Components/ProductsReview/ProductsReview.js
@@ -1,21 +1,29 @@
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Button, Card, CardGroup } from "react-bootstrap";
+import "./ProductsReview.css";
 
 const ProductsReview = (props) => {
   const { name, image, comment, rating } = props.product;
   return (
     <div className="col-md-4">
-      <Card>
-        <Card.Img width="50px" height="100px" className="p-3 rounded-circle" src={image.url} />
+      <Card className="align-items-center">
+        <Card.Img
+          style={{ width: "100px" }}
+          className=" img-style rounded-circle"
+          src={image.url}
+        />
         <Card.Body>
           <Card.Title>{name}</Card.Title>
-          <Card.Text className="text-start">{comment.slice(0, 150)}</Card.Text>
-          <div className="d-flex justify-content-between">
-            <span>Rating: {rating}</span>
-            <button title={comment} className="border-0 py-1 px-2 btn-color text-white rounded ">
-              More Details
-            </button>
+          <div className="mb-4">
+            <FontAwesomeIcon className="text-warning icons" icon={faStar} />
+            <FontAwesomeIcon className="text-warning icons" icon={faStar} />
+            <FontAwesomeIcon className="text-warning icons" icon={faStar} />
+            <FontAwesomeIcon className="text-warning icons" icon={faStar} />
+            <FontAwesomeIcon className="text-warning icons" icon={faStar} />
           </div>
+          <Card.Text className="">{comment.slice(0, 150)}</Card.Text>
         </Card.Body>
       </Card>
     </div>
