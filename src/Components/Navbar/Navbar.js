f891e41f75c852634cafa71c49e6a3ee10934f59
abdurrahmancc <personalbusinessmail63@gmail.com>
2022-04-05 18:01:54 +0600

    not found page design added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,36 @@
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Nav } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const { pathname } = useLocation();
+  console.log("emtty", pathname);
   return (
     <>
-      <nav className="nav-color py-4">
+      <nav
+        /* style={
+          pathname.includes(" ")
+            ? { display: "block" }
+            :  pathname.includes("home")
+            ? { display: "block" }
+            : pathname.includes("reviews")
+            ? { display: "block" }
+            : pathname.includes("dashboard")
+            ? { display: "block" }
+            : pathname.includes("blogs")
+            ? { display: "block" }
+            : pathname.includes("about")
+            ? { display: "block" }
+            : { display: "none" }
+        } */
+        className="nav-color py-4"
+      >
+        <NavLink to={-1}>
+          <FontAwesomeIcon className="text-white fs-4 me-4" icon={faArrowLeft}></FontAwesomeIcon>
+        </NavLink>
         <NavLink
           to={"/home"}
           className={({ isActive }) =>
@@ -56,6 +80,9 @@ const Navbar = () => {
           }
         >
           About
+        </NavLink>
+        <NavLink to={1}>
+          <FontAwesomeIcon className="text-white fs-4 ms-4" icon={faArrowRight}></FontAwesomeIcon>
         </NavLink>
       </nav>
     </>
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,10 +1,26 @@
+import {
+  faAngleLeft,
+  faArrowLeft,
+  faArrowLeftLong,
+  faBackspace,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import "./NotFound.css";
 
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>404</h1>
-      <h3>Your search did not match any documents.</h3>
+    <div className="styel404">
+      <button
+        className="style-btn text-white btn-color border-0 fs-4 "
+        onClick={() => navigate(-1)}
+      >
+        {" "}
+        <FontAwesomeIcon className="me-2" icon={faArrowLeft}></FontAwesomeIcon>
+        Back
+      </button>
     </div>
   );
 };
